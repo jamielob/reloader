34caf35fdb3625c6af3c1a1b36274675cbfcbb16
Jamie Loberman <jamie@beautifulcoding.com>
2016-04-19 00:53:43 -0700

    Bump to 1.0.0

--- packages/reloader/package.js
+++ packages/reloader/package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'jamielob:reloader',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'More control over hot code push reloading',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jamielob/reloader/tree/master/packages/reloader',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
