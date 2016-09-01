b0242890819595d6f737f7571c42134b6f3fb52d
Loren Sands-Ramshaw <lorensr@gmail.com>
2016-09-01 19:45:43 -0400

    Autoformat

--- reloader.js
+++ reloader.js
@@ -2,183 +2,183 @@ const launchScreen = LaunchScreen.hold();
 
 Reloader = {
 
-	_options: {},
+  _options: {},
 
-	configure(options) {
-		check(options, {
-			check: Match.Optional(Match.OneOf('everyStart', 'firstStart', false)),
-			checkTimer: Match.Optional(Match.Integer),
-			refresh: Match.Optional(Match.OneOf('startAndResume', 'start', 'instantly')),
-			idleCutoff: Match.Optional(Match.Integer),
-			launchScreenDelay: Match.Optional(Match.Integer),
-		});
-		
-		_.extend(this._options, options);
-	},
+  configure(options) {
+    check(options, {
+      check: Match.Optional(Match.OneOf('everyStart', 'firstStart', false)),
+      checkTimer: Match.Optional(Match.Integer),
+      refresh: Match.Optional(Match.OneOf('startAndResume', 'start', 'instantly')),
+      idleCutoff: Match.Optional(Match.Integer),
+      launchScreenDelay: Match.Optional(Match.Integer),
+    });
+
+    _.extend(this._options, options);
+  },
 
   updateAvailable: new ReactiveVar(false),
 
-	reload() {
-		// Show the splashscreen
-		navigator.splashscreen.show();
+  reload() {
+    // Show the splashscreen
+    navigator.splashscreen.show();
 
-		// Set the refresh flag
-		localStorage.setItem('reloaderWasRefreshed', Date.now());
+    // Set the refresh flag
+    localStorage.setItem('reloaderWasRefreshed', Date.now());
 
-		// Reload the page
-		window.location.replace(window.location.href);
-	},
+    // Reload the page
+    window.location.replace(window.location.href);
+  },
 
 
-	// Should check if a cold start and (either everyStart is set OR firstStart
-	// is set and it's our first start)
-	_shouldCheckForUpdateOnStart() {
-		const isColdStart = !localStorage.getItem('reloaderWasRefreshed');
-		return isColdStart &&
-			(
-				this._options.check === 'everyStart' ||
-					(
-						this._options.check === 'firstStart' &&
-							!localStorage.getItem('reloaderLastStart')
-					)
-			);
-	},
+  // Should check if a cold start and (either everyStart is set OR firstStart
+  // is set and it's our first start)
+  _shouldCheckForUpdateOnStart() {
+    const isColdStart = !localStorage.getItem('reloaderWasRefreshed');
+    return isColdStart &&
+    (
+      this._options.check === 'everyStart' ||
+      (
+        this._options.check === 'firstStart' &&
+        !localStorage.getItem('reloaderLastStart')
+      )
+    );
+  },
 
-	// Check if the idleCutoff is set AND we exceeded the idleCutOff limit AND the everyStart check is set
-	_shouldCheckForUpdateOnResume() {
-		// In case a pause event was missed, assume it didn't make the cutoff
-		if (!localStorage.getItem('reloaderLastPause')) {
-			return false;
-		}
-		
-		// Grab the last time we paused
-		const lastPause = Number(localStorage.getItem('reloaderLastPause'));
+  // Check if the idleCutoff is set AND we exceeded the idleCutOff limit AND the everyStart check is set
+  _shouldCheckForUpdateOnResume() {
+    // In case a pause event was missed, assume it didn't make the cutoff
+    if (!localStorage.getItem('reloaderLastPause')) {
+      return false;
+    }
 
-		// Calculate the cutoff timestamp
-		const idleCutoffAt = Number( Date.now() - this._options.idleCutoff );
+    // Grab the last time we paused
+    const lastPause = Number(localStorage.getItem('reloaderLastPause'));
 
-		return (
-			this._options.idleCutoff &&
-				lastPause < idleCutoffAt &&
-				this._options.check === 'everyStart'
-		);
-	},
+    // Calculate the cutoff timestamp
+    const idleCutoffAt = Number( Date.now() - this._options.idleCutoff );
 
-	_waitForUpdate(computation) {
-		// Check if we have a HCP after the check timer is up
-		Meteor.setTimeout(() => {
+    return (
+      this._options.idleCutoff &&
+      lastPause < idleCutoffAt &&
+      this._options.check === 'everyStart'
+    );
+  },
 
-			// If there is a new version available
-			if (this.updateAvailable.get()) {
+  _waitForUpdate(computation) {
+    // Check if we have a HCP after the check timer is up
+    Meteor.setTimeout(() => {
 
-				this.reload();
+      // If there is a new version available
+      if (this.updateAvailable.get()) {
 
-			} else {
+        this.reload();
 
-				// Stop waiting for update
-				if (computation) {
-					computation.stop()
-				}
+      } else {
 
-				launchScreen.release();
+        // Stop waiting for update
+        if (computation) {
+          computation.stop()
+        }
 
-			}
-			
-		}, this._options.checkTimer );
-	},
+        launchScreen.release();
 
-	_checkForUpdate() {
-		if (this.updateAvailable.get()) {
+      }
 
-			// Check for an even newer update
-			this._waitForUpdate()
+    }, this._options.checkTimer );
+  },
 
-		} else {
-			
-			// Wait until update is available, or give up on timeout
-			Tracker.autorun((c) => {
+  _checkForUpdate() {
+    if (this.updateAvailable.get()) {
 
-				if (this.updateAvailable.get()) {
-					this.reload();
-				}
+      // Check for an even newer update
+      this._waitForUpdate()
 
-				this._waitForUpdate(c)
+    } else {
 
-			});
+      // Wait until update is available, or give up on timeout
+      Tracker.autorun((c) => {
 
-		}
-	},
+        if (this.updateAvailable.get()) {
+          this.reload();
+        }
 
-	_onPageLoad() {
-		if (this._shouldCheckForUpdateOnStart()) {
-			
-			this._checkForUpdate();
-			
-		} else {
-			
-			Meteor.setTimeout(function() {
+        this._waitForUpdate(c)
 
-				launchScreen.release();
+      });
 
-				// Reset the reloaderWasRefreshed flag
-				localStorage.removeItem('reloaderWasRefreshed');
+    }
+  },
 
-			}, this._options.launchScreenDelay); // Short delay helps with white flash
+  _onPageLoad() {
+    if (this._shouldCheckForUpdateOnStart()) {
 
-		}
-	},
+      this._checkForUpdate();
 
-	_onResume() {
-		const shouldCheck = this._shouldCheckForUpdateOnResume();
+    } else {
 
-		localStorage.removeItem('reloaderLastPause');
+      Meteor.setTimeout(function() {
 
-		if (shouldCheck) {
-			
-			navigator.splashscreen.show();
+        launchScreen.release();
 
-			this._checkForUpdate();
+        // Reset the reloaderWasRefreshed flag
+        localStorage.removeItem('reloaderWasRefreshed');
 
-			// If we don't need to do an additional check
-		} else {
+      }, this._options.launchScreenDelay); // Short delay helps with white flash
 
-			// Check if there's a new version available already AND we need to refresh on resume
-			if ( this.updateAvailable.get() && this._options.refresh === 'startAndResume' ) {
+    }
+  },
 
-				this.reload();
+  _onResume() {
+    const shouldCheck = this._shouldCheckForUpdateOnResume();
 
-			} 
+    localStorage.removeItem('reloaderLastPause');
 
-		}
-	},
+    if (shouldCheck) {
 
-	_onMigrate() {
-		if (this._options.refresh === 'instantly') {
+      navigator.splashscreen.show();
 
-			this.reload();
+      this._checkForUpdate();
 
-			// return [true, {}];
+      // If we don't need to do an additional check
+    } else {
 
-		} else {
+      // Check if there's a new version available already AND we need to refresh on resume
+      if ( this.updateAvailable.get() && this._options.refresh === 'startAndResume' ) {
 
-			// Set the flag
-			this.updateAvailable.set(true);
+        this.reload();
 
-			// Don't refresh yet
-			return false;
+      }
 
-		}
-	}
+    }
+  },
+
+  _onMigrate() {
+    if (this._options.refresh === 'instantly') {
+
+      this.reload();
+
+      // return [true, {}];
+
+    } else {
+
+      // Set the flag
+      this.updateAvailable.set(true);
+
+      // Don't refresh yet
+      return false;
+
+    }
+  }
 
 };
 
 // Set the defaults
 Reloader.configure({
-	check: 'everyStart',
-	checkTimer: 3000,
-	refresh: 'startAndResume',
-	idleCutoff: 1000 * 60 * 10, // 10 minutes
-	launchScreenDelay: 100
+  check: 'everyStart',
+  checkTimer: 3000,
+  refresh: 'startAndResume',
+  idleCutoff: 1000 * 60 * 10, // 10 minutes
+  launchScreenDelay: 100
 });
 
 
@@ -189,7 +189,7 @@ localStorage.setItem('reloaderLastStart', Date.now());
 
 // Watch for the app resuming
 document.addEventListener("resume", function () {
-	Reloader._onResume();
+  Reloader._onResume();
 }, false);
 
 
@@ -197,24 +197,23 @@ localStorage.removeItem('reloaderLastPause');
 
 // Watch for the device pausing
 document.addEventListener("pause", function() {
-	// Save to localStorage
-	localStorage.setItem('reloaderLastPause', Date.now());
+  // Save to localStorage
+  localStorage.setItem('reloaderLastPause', Date.now());
 }, false);
 
 
-// Capture the reload 
+// Capture the reload
 Reload._onMigrate(function (retry) {
-	Reloader._onMigrate();
+  Reloader._onMigrate();
 });
 
 
 // Update available template helper
 Template.registerHelper("updateAvailable", function() {
-	return Reloader.updateAvailable.get();
+  return Reloader.updateAvailable.get();
 });
 
 // Update available event
 $(document).on('click', '[reloader-update]', function(event) {
-	Reloader.reload();
+  Reloader.reload();
 });
-
