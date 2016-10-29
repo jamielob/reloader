97bf11a2900b197e34a61caaaa7d15f73bd7e3f0
Ahmed Hassan <a.hassan@buismed.de>
2016-10-29 20:29:04 +0200

    bump package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jamielob:reloader',
-  version: '1.2.2',
+  version: '1.2.3',
   summary: 'More control over hot code push reloading',
   git: 'https://github.com/jamielob/reloader/',
   documentation: 'README.md'
